f5bf829daa79b91aecce0b1c8395d52ce40f7b24
AdrianVazquezTorresLAP <adrian.vazquez.employment@gmail.com>
2026-03-24 15:30:41 -0600

    Se modificó el archivo .gitignore para excluir txt y el archivo que define la estructura de las carpetas

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -2,6 +2,9 @@ from pydantic import BaseModel, Field
 
 
 class BankTransaction(BaseModel):
+    # Agregamos Field() a algunos campos para agregar reglas extra y/o contexto de las variables
+    # Los 3 puntos significan que el campo DEBE SER OBLIGATORIO, si no se envía mínimo esta información
+    # el código del modelo ni siquiera se ejecuta.
     step: int = Field(..., description="Hour of transaction (1 step = 1 hour)")
     type: str = Field(..., description="Type of transaction")
     amount: float = Field(..., gt=0, description="Amount of the transaction (must be greater than 0)")
