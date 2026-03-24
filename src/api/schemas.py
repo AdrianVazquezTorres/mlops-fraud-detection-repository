a81371716b6fc09ad720883b81ade862159bf7e0
AdrianVazquezTorresLAP <adrian.vazquez.employment@gmail.com>
2026-03-24 13:06:27 -0600

    Se corrigió el parámetro step de la clase BankTransaction del archivo schemas.py. Tenía asignado el datatype string y fue ccambiado por int

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field
 
 
 class BankTransaction(BaseModel):
-    step: str = Field(..., description="The step of the transaction")
+    step: int = Field(..., description="Hour of transaction (1 step = 1 hour)")
     type: str = Field(..., description="Type of transaction")
     amount: float = Field(..., gt=0, description="Amount of the transaction (must be greater than 0)")
     oldbalanceOrg: float
