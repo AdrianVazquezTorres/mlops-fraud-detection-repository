a335661708ac4cdeee06eaff8a936fa7f1bd7407
AdrianVazquezTorresLAP <adrian.vazquez.employment@gmail.com>
2026-05-20 12:33:49 -0600

    producer.py: modificado para enviar registros constantemente con while loop

--- src/streaming/producer.py
+++ src/streaming/producer.py
@@ -2,12 +2,18 @@ import pandas as pd
 import json
 import time
 import os
+import logging
 from confluent_kafka import Producer
 from pathlib import Path
 # Para revisar esquema
 from src.api.schemas import BankTransaction
 
+# Configuración de los logs para producción (reemplaza los print convencionales)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Dirección del broker ()
 broker = os.getenv("KAFKA_BROKER", "kafka:9092")
+topic_name = "fraud-transaction-stream"  # Nombre del tópico de Kafka
 
 # 1. Configuración del productor
 # 'bootstrap.servers': Dirección del contenedor kafka
@@ -26,12 +32,12 @@ producer = Producer(conf)
 
 def delivery_report(err, msg):
     """
-    Se ejecuta cuando Kafka confirma que recibió el mensaje o si hubo un error.
+    Se ejecuta de forma asíncrona por cada mensaje cuando kafka confirma su recepción
     """
     if err is not None:
         print(f"❌ Error al entregar mensaje: {err}")
-    else:
-        print(f"✅ Transacción enviada a {msg.topic()} [Partition: {msg.partition()}]")
+    # else:
+        # print(f"✅ Transacción enviada a {msg.topic()} [Partition: {msg.partition()}]")
         # Mostramos solo los primeros 20 caracteres del mensaje para no saturar la terminal
         # print(f"✅ Enviado: {msg.value().decode('utf-8')[:50]}...")
 
@@ -42,37 +48,64 @@ def run_producer():
     file_path = Path("data/daily_incoming_parquet/data_2026_04_15.parquet")
 
     if not file_path.exists():
-        print(f"🚨 No se encontró el archivo en {file_path}")
+        print(f"🚨 No se encontró el archivo/ruta en {file_path}")
         return
 
+    logging.info('Cargando datos históricos en memoria...')
     df = pd.read_parquet(file_path)
     # Tomamos una muestra para la prueba del esquema
-    sample_data = df.head(50).to_dict(orient="records")
 
-    print("🚀 Iniciando streaming con validación de esquema...")
+    logging.info('🚀 Iniciando simulación de ingesta: 50 transacciones por segundo...')
 
     try:
-        for row in sample_data:
-            try:
-                # Validamos que la fila cumpla con BankTransaction
-                valid_tx = BankTransaction(**row)
-                # Convertimos a JSON (usamos .dict() o .model_dump() según la versión de Pydantic)
-                payload = valid_tx.model_dump_json().encode('utf-8')
+        # Bucle infinito para simular un flujo 24/7
+        while True:
+            for index, row in df.iterrows():
+                transaction_dict = row.to_dict()
+                json_data = json.dumps(transaction_dict)
 
+                # Enviar mensaje al tópico
                 producer.produce(
-                    topic='transactions',
-                    value=payload,
+                    topic=topic_name,
+                    value=json_data.encode('utf-8'),
                     callback=delivery_report
                 )
+
+                # Permite al producer manejar eventos en segundo plano (como el delivery)
                 producer.poll(0)
-                time.sleep(1)  # delay
-            except Exception as e:
-                print(f"⚠️ Fila inválida saltada. Error: {e}")
-                continue
+
+                # Control de Tasa (Rate Limiting)
+                # 1 segundo / 50 = 0.02 segundos de espera entra cada transacción
+                logging.info(f"Transacción {transaction_dict['type']} enviada con {transaction_dict['amount']} cantidad")
+                time.sleep(0.02)
+            logging.info('🔄 Fin del lote alcanzado. Reiniciando el archivo para mantener el streaming...')
+
+        # for row in sample_data:
+        #     try:
+        #         # Validamos que la fila cumpla con BankTransaction
+        #         valid_tx = BankTransaction(**row)
+        #         # Convertimos a JSON (usamos .dict() o .model_dump() según la versión de Pydantic)
+        #         payload = valid_tx.model_dump_json().encode('utf-8')
+
+        #         producer.produce(
+        #             topic='transactions',
+        #             value=payload,
+        #             callback=delivery_report
+        #         )
+        #         producer.poll(0)
+        #         time.sleep(1)  # delay
+        #     except Exception as e:
+        #         print(f"⚠️ Fila inválida saltada. Error: {e}")
+        #         continue
     except KeyboardInterrupt:
-        print("\n⛔ Streaming detenido ⛔")
+        logging.warning('🛑 Simulación detenida manualmente por el usuario (KeyboardInterrupt).')
+    except Exception as e:
+        logging.error(f'⚠️ Error inesperado: {e}')
     finally:
+        # Este paso es vital para no perder mensajes que quedaron en memoria antes de apagar
+        logging.info('Vaciando la cola de mensajes restantes (flush)...')
         producer.flush()
+        logging.info("Productor apagado de forma segura.")
 
 
 if __name__ == "__main__":
